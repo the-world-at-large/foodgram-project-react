2721ddca3c7ccd6712e0ba8859a132a395df80c4
the-world-at-large <artem-abramov-2002@mail.ru>
2024-01-22 21:23:13 +0200

    ...

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -15,7 +15,8 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='django').split()
 
 CSRF_COOKIE_SECURE = os.getenv('CSRF_COOKIE_SECURE', 'false').lower() == 'true'
 CSRF_COOKIE_SAMESITE = os.getenv('CSRF_COOKIE_SAMESITE')
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split()
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
